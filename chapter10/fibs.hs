00582b82afe613eeaacb17b91dba84d229ad57e9
zooeymiller <zooeyxmiller@gmail.com>
2018-07-14 13:46:16 +0100

    working on ch10 ex

---
+++ chapter10/fibs.hs
@@ -0,0 +1,10 @@
+fibs = 1 : scanl (+) 1 fibs
+
+first20Fibs = take 20 fibs
+
+fibsLessThan100 = takeWhile (<100) fibs
+
+factorialRec 0 = 1
+factorialRec n = n * (factorialRec (n - 1))
+
+fact = scanl (*) 1 [2..]
