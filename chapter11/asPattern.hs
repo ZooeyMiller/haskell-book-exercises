13553f7cf705fb478c2eeed894eb0713afced593
Zooey Miller <zooeyxmiller@gmail.com>
2018-07-19 13:10:13 +0100

    ch11 asPattern

---
+++ chapter11/asPattern.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+import Data.Char
+
+isSubseqOf :: (Eq a) 
+                => [a]
+                -> [a] 
+                -> Bool
+
+isSubseqOf [] _ = True
+isSubseqOf _ [] = False
+isSubseqOf t@(x:xs) (y:ys) = if x == y then isSubseqOf xs ys else isSubseqOf t ys
+
+capitalizeWords :: String -> [(String, String)]
+capitalizeWords = map go . words
+    where go t@(x:xs) = (t, toUpper x : xs)
