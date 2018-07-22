f5b03046bea2e2b79802c85f28c11586e14f81a0
zooeymiller <zooeyxmiller@gmail.com>
2018-07-22 21:49:01 +0100

    add countVowels

--- chapter12/stringProcessing.hs
+++ chapter12/stringProcessing.hs
@@ -20,3 +20,18 @@ replace toReplace replaceWith str = go [] (words str) toReplace replaceWith
         where
             go res [] _ _        = unwords res
             go res (x:xs) rep wi = if x == rep then go (res ++ [wi]) xs rep wi else go (res ++ [x]) xs rep wi
+
+countTheBeforeVowel :: String -> Integer
+countTheBeforeVowel str = go 0 (words str) False
+            where
+                go res [] _ = res
+                go res (x:xs) lastWasThe = go (res+(pass lastWasThe (head x))) xs (x=="the")
+                pass lastWasThe char = if (lastWasThe && isVowel char) then 1 else 0
+
+
+
+isVowel :: Char -> Bool
+isVowel = flip elem $ "aeiou"
+
+countVowels :: String -> Int
+countVowels = length . filter (&&True) . map isVowel
