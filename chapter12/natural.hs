2077a8bd1776e7fb54917e7ecd332220c86bad37
Zooey Miller <zooeyxmiller@gmail.com>
2018-07-23 15:26:51 +0100

    ch12 natural

---
+++ chapter12/natural.hs
@@ -0,0 +1,17 @@
+data Nat =
+    Zero
+    | Succ Nat
+    deriving (Eq, Show)
+
+natToInteger :: Nat -> Integer 
+natToInteger Zero = 0
+natToInteger (Succ x) = 1 + (natToInteger x)
+
+
+integerToNat :: Integer -> Maybe Nat 
+integerToNat int
+            | int < 0 = Nothing
+            | otherwise = Just (go int Zero)
+                where 
+                    go 0 res = res
+                    go n res = go (n-1) (Succ res)
