cd46c922612901b8d56887faf9ad2cd015a47524
zooeymiller <zooeyxmiller@gmail.com>
2018-10-12 19:32:32 +0100

    ch18/myBind

---
+++ chapter18/myBind.hs
@@ -0,0 +1,7 @@
+import           Control.Monad (join)
+
+-- join :: Monad m => m (m a) -> m a
+-- write bind in terms of fmap and join
+bind :: Monad m => (a -> m b) -> m a -> m b
+bind = (join .) . fmap
+-- bind f m = join $ fmap f m
