612872a1e14b95e4c22cd4500d271787d18a86ac
zooeymiller <zooeyxmiller@gmail.com>
2018-10-13 07:19:31 +0100

    ch18/twiceWhenEven

---
+++ chapter18/twiceWhenEven.hs
@@ -0,0 +1,12 @@
+twiceWhenEven :: [Integer] -> [Integer]
+twiceWhenEven xs = do
+    x <- xs
+    if even x
+        then [x*x, x*x]
+        else []
+
+-- Given twiceWhenEven [1..3] expected result is [4, 4]
+
+twiceWhenEvenDesugared :: [Integer] -> [Integer]
+twiceWhenEvenDesugared xs = do
+    xs >>= (\x -> if even x then [x*x, x*x] else [])
