7c622034789e3d2c3a70e3535bbbb8a5e573c282
Zooey Miller <zooeyxmiller@gmail.com>
2018-10-22 13:57:31 +0100

    chapter22/warmingUp

---
+++ chapter22/warmingUp.hs
@@ -0,0 +1,25 @@
+import Data.Char
+
+cap :: [Char] -> [Char]
+cap xs = map toUpper xs 
+
+rev :: [Char] -> [Char]
+rev xs = reverse xs
+
+composed :: [Char] -> [Char]
+composed = rev . cap 
+
+fmapped :: [Char] -> [Char]
+fmapped = fmap rev cap
+
+tupled :: [Char] -> ([Char], [Char])
+tupled = (,) <$> rev <*> cap
+
+tupledDo :: [Char] -> ([Char], [Char])
+tupledDo = do
+    a <- rev
+    b <- cap
+    return (a, b)
+
+tupledBind :: [Char] -> ([Char], [Char])
+tupledBind s = rev (>>=) (\x y -> (cap y, x))
